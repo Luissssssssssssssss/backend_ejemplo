bfd98f76e0209d6b2826470ef45bd60e60202f68
Luis <barriosluis@hotmail.com>
2024-08-19 17:53:59 -0600

    Versionamiento Java

---
+++ Ejemplo1/src/main/java/Ejemplo1_1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Luis
+ */
+public class Ejemplo1_1 {
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hola Mundo");
+    }
+}
